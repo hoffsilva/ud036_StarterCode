701a5e650eb6e5a87a9db7ee0efe74865a78313e
Hoff Silva <hoff.henry@gmail.com>
2018-01-24 22:56:06 -0200

    Adjusting css.

---
+++ media.py
@@ -0,0 +1,12 @@
+class Movie():
+    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url, director, writers, stars):
+        self.title = title
+        self.stoyline = storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+        self.director = director
+        self.writers = writers
+        self.stars = stars
+
+
+
