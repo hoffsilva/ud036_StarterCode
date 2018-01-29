c1e204d04fac93f4effd1392377c7059d59ee603
Hoff Silva <hoff.silva@castgroup.com.br>
2018-01-29 13:00:48 -0200

    Pep8 bug fixes.

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,9 +3,18 @@ import fresh_tomatoes
 import requests
 import json
 
-api_key = ""
+default_url = "https://api.themoviedb.org/3/movie/popular?"
+api_key = "api_key=edacb7eeacf9c2be67d32057dc199a0f"
+# set the default idiom to get info about movies
+lang_set = "&language=en-US"
+number_page = "&page=1"
+full_url = default_url + api_key + lang_set + number_page
 
-list_of_movies = requests.get('https://api.themoviedb.org/3/movie/popular?api_key='+api_key+'&language=en-US&page=1')
+youtube_url = "https://www.youtube.com/watch?v="
+
+movies_url = default_url+"popular"
+
+list_of_movies = requests.get(full_url)
 
 if list_of_movies.status_code != 200:
     # This means something went wrong.
@@ -13,18 +22,21 @@ if list_of_movies.status_code != 200:
 
 array_of_movies = []
 
+
 # get poster url
 def get_poster_url_by(poster_path):
     poster_url = "https://image.tmdb.org/t/p/w600_and_h900_bestv2"+poster_path
     return poster_url
 
+
 # get trailer url
 def get_trailer_url_by(movie_id):
-    trailers_api_url = "https://api.themoviedb.org/3/movie/"+str(movie_id)+"/videos?api_key="+api_key+"&language=en-US"
+    trailers_api_url = movies_url+str(movie_id)+"/videos?"+api_key+lang_set
     trailer_object = requests.get(trailers_api_url)
     print(trailer_object)
-    trailer_url = "https://www.youtube.com/watch?v=" + str(trailer_object.json()["results"][0]["key"])
+    trailer_url = youtube_url + str(trailer_object.json()["results"][0]["key"])
     return trailer_url
+
 
 # create a movie object
 def get_movie_data(movie):
@@ -47,10 +59,12 @@ def get_movie_data(movie):
     )
     return data_movie
 
+
 # populates the array with movie objects
 json_list = json.loads(list_of_movies.text)["results"]
 
 for movie_item in json_list:
     array_of_movies.append(get_movie_data(movie_item))
+
 
 fresh_tomatoes.open_movies_page(array_of_movies)
--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -102,7 +102,6 @@ main_page_content = '''
         </div>
       </div>
     </div>
-    
     <!-- Main Page Content -->
     <div class="container">
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -146,6 +145,7 @@ movie_tile_content = '''
 </div>
 '''
 
+
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
@@ -157,27 +157,26 @@ def create_movie_tiles_content(movies):
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
-            movie_title = movie.title,
-            poster_image_url = movie.poster_image_url,
-            trailer_youtube_id = trailer_youtube_id,
-            movie_storyline = movie.stoyline,
-            vote_average = movie.vote_average,
-            popularity = movie.popularity,
-            release_date = movie.release_date
+            movie_title=movie.title,
+            poster_image_url=movie.poster_image_url,
+            trailer_youtube_id=trailer_youtube_id,
+            movie_storyline=movie.stoyline,
+            vote_average=movie.vote_average,
+            popularity=movie.popularity,
+            release_date=movie.release_date
         )
     return content
 
+
 def open_movies_page(movies):
-  # Create or overwrite the output file
-  output_file = open('fresh_tomatoes.html', 'w')
-
-  # Replace the placeholder for the movie tiles with the actual dynamically generated content
-  rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
-
-  # Output the file
-  output_file.write(main_page_head + rendered_content)
-  output_file.close()
-
-  # open the output file in the browser
-  url = os.path.abspath(output_file.name)
-  webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
+    # Create or overwrite the output file
+    output_file = open('fresh_tomatoes.html', 'w')
+    # Replace the placeholder for the movie tiles with the actual dynamically generated content
+    rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
+    # Output the file
+    output_file.write(main_page_head + rendered_content)
+    output_file.close()
+    # open the output file in the browser
+    url = os.path.abspath(output_file.name)
+    # open in a new tab, if possible
+    webbrowser.open('file://' + url, new=2)
